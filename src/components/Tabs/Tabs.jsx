806fed84b89f8791c39fef54b3158a8767896c8d
Sertavr <pampuh.net@gmail.com>
2025-12-02 10:39:08 +0200

    add task solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,27 @@
-export const Tabs = () => {};
+export const Tabs = ({ currentTab, tabs, onTabSelected }) => {
+  return (
+    <ul>
+      {tabs.map(tab => (
+        <li
+          className={currentTab === tab.title.at(-1) ? 'is-active' : ''}
+          data-cy="Tab"
+          key={tab.id}
+        >
+          <a
+            onClick={() => {
+              if (currentTab === tab.title.at(-1)) {
+                return;
+              }
+
+              onTabSelected(`${tab.title.at(-1)}`);
+            }}
+            href={`#${tab.id}`}
+            data-cy="TabLink"
+          >
+            {tab.title}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
